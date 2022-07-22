e7e04e1ebb2ea363f6eb7873897471dc980a6b42
Matheus Melo <dev.matheus.melo@gmail.com>
2022-07-22 15:49:09 -0300

    feat(component): sidebar component created
    Sidebar helps to navigate through the app

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,19 @@
 import type { NextPage } from 'next';
+import { Flex } from '@chakra-ui/react';
 
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 const Dashboard: NextPage = () => {
-  return <Header />;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Dashboard;
