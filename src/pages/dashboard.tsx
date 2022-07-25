8c0da0b3b4aa55e2056aab39992f925fc650576a
Matheus Melo <dev.matheus.melo@gmail.com>
2022-07-25 12:34:29 -0300

    feat(page): responsive dashboard
    Chart box padding change in different window size

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -76,7 +76,7 @@ const Dashboard: NextPage = () => {
           minChildWidth="320px"
           alignItems="flex-start"
         >
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={['6', '8']} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Inscritos da semana
             </Text>
